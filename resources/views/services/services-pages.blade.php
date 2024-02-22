5ccd20e50ece49a405063bc881f459ca59ba6374
Ghulam Mustafa <gmustafa@TOOBITECH.LOCAL>
2024-02-22 23:30:20 +0500

    about and service

---
+++ resources/views/services/services-pages.blade.php
@@ -0,0 +1,172 @@
+<section class="">
+    <div class="container-fluid">
+        <div class="row" data-aos="zoom-in">
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                    <div style="position:relative">
+                    <img src="images/post-1.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Key Match</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"> <a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-2.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Home Finders</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>Lorem Ipsum i simply dummy text of the printing and types</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-3.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Dream Space</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-4.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Place Perfect</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-5.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Home Vision</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-6.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Move Mate</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-5.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Home Vision</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-5.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Home Vision</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right" style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-md-6 col-lg-4 mt-5">
+                <div class="post-items">
+                <div style="position:relative">
+                    <img src="images/post-5.png" class="img-fluid w-100 img-radius" srcset="" />
+                    <div class="panel">
+                        <i class="fa-solid fa-house-lock icon-size"></i>
+                    </div>
+                    </div>
+                    <div class="content-post px-4 py-4 mt-1">
+                        <h6>Home Vision</h6>
+                        <div class="d-flex align-items-end">
+                            <div><p>It is a long established fact that a reader will be distracted</p></div>
+                            <div class="mb-2" id="heart-circle"><a href="services-details.php"> <i class="fa-solid fa-arrow-right "  style="font-size: 20px;"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
 @include('header.header')
-
+@yield('main')
 @include('footer.footer')
+
+
+
+{{-- @include('about.about-about')
+
+@include('about.about-faqs') --}}
